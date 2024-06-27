a9237bbe1c5d94db90a4908463cfe5b683735193
uttam172 <uttamdobariya17@gmail.com>
2024-06-27 18:43:57 +0530

    completed

---
+++ models/user2.js
@@ -0,0 +1,34 @@
+import { Schema, model, models } from 'mongoose'
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        unique: [true, 'Email already exists!'],
+        required: [true, 'Email is required!'],
+    },
+    username: {
+        type: String,
+        required: [true, 'Username is required!'],
+        match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
+    },
+    image: {
+        type: String,
+    },
+    googleId: {
+        type: String,
+    }
+})
+
+UserSchema.statics.fromGoogleAuthResponse = function (authResponse) {
+    const { email, given_name, family_name, picture } = authResponse
+    return this.create({
+        email,
+        username: `${given_name} ${family_name}`,
+        image: picture,
+        googleId: authResponse.sub,
+    })
+}
+
+const User = models.User || model("User", UserSchema)
+
+export default User
